cf0d18261fba7c9b7f893def08cb3a48fb90a6fa
Theo Pouris <theopouris@proton.me>
2023-07-04 02:11:44 +0300

    Update reminders.dart

--- lib/reminders.dart
+++ lib/reminders.dart
@@ -1,113 +1,281 @@
 import 'package:flutter/material.dart';
-import 'editrems.dart';
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Reminder {
+  final String title;
+  final String frequency;
+  bool isActive;
+
+  Reminder({
+    required this.title,
+    required this.frequency,
+    this.isActive = true,
+  });
+
+  factory Reminder.fromJson(Map<String, dynamic> json) {
+    return Reminder(
+      title: json['title'],
+      frequency: json['frequency'],
+      isActive: json['isActive'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'frequency': frequency,
+      'isActive': isActive,
+    };
+  }
+}
 
 class RemindersScreen extends StatefulWidget {
-  const RemindersScreen({super.key});
+  const RemindersScreen({Key? key}) : super(key: key);
 
   @override
   _RemindersScreenState createState() => _RemindersScreenState();
 }
 
 class _RemindersScreenState extends State<RemindersScreen> {
-  List<Reminder> reminders = [
-    Reminder(title: 'Reminder 1', frequency: 'Daily', isActive: true),
-    Reminder(title: 'Reminder 2', frequency: 'Weekly', isActive: false),
-    Reminder(title: 'Reminder 3', frequency: 'Monthly', isActive: true),
-  ];
+  List<Reminder> reminders = [];
 
-  void toggleReminder(int index) {
-    setState(() {
-      reminders[index].isActive = !reminders[index].isActive;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _loadReminders();
   }
 
-  void editReminder(int index) async {
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => EditReminderScreen(
-          reminderTitle: reminders[index].title,
-          reminderFrequency: reminders[index].frequency,
-        ),
-      ),
-    );
-
-    if (result != null) {
+  void _loadReminders() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? remindersJson = prefs.getStringList('reminders');
+    if (remindersJson != null) {
       setState(() {
-        reminders[index].title = result['title'];
-        reminders[index].frequency = result['frequency'];
+        reminders = remindersJson
+            .map((item) => Reminder.fromJson(json.decode(item)))
+            .toList();
       });
     }
+  }
+
+  void _saveReminders() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> remindersJson =
+        reminders.map((reminder) => json.encode(reminder.toJson())).toList();
+    await prefs.setStringList('reminders', remindersJson);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Reminders'),
+        title: Text('Reminders'),
       ),
       body: ListView.builder(
         itemCount: reminders.length,
         itemBuilder: (context, index) {
-          final reminder = reminders[index];
-          return ListTile(
-            tileColor:
-                reminder.isActive ? const Color(0xFF1D1A22) : Colors.grey[400],
-            title: Text(
-              reminder.title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+          Reminder reminder = reminders[index];
+          return Dismissible(
+            key: Key(reminder.title),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Icon(Icons.delete, color: Colors.white),
             ),
-            subtitle: Text(
-              reminder.frequency,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 14,
+            onDismissed: (direction) {
+              setState(() {
+                reminders.removeAt(index);
+              });
+              _saveReminders();
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Container(
+                color: Colors.grey[900],
+                margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: ListTile(
+                  title: Text(
+                    reminder.title,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  subtitle: Text(
+                    reminder.frequency,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  trailing: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        reminder.isActive = !reminder.isActive;
+                      });
+                      _saveReminders();
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        reminder.isActive
+                            ? Icons.notifications_active
+                            : Icons.notifications_off,
+                        color: Colors.grey[900],
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    _navigateToAddReminderScreen(
+                        reminder: reminder, index: index);
+                  },
+                ),
               ),
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  onPressed: () => toggleReminder(index),
-                  icon: Icon(
-                    reminder.isActive ? Icons.toggle_on : Icons.toggle_off,
-                    color: Colors.white,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () => editReminder(index),
-                  icon: const Icon(
-                    Icons.edit,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // TODO: Implement add reminder functionality
+          _navigateToAddReminderScreen();
         },
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
     );
   }
+
+  void _navigateToAddReminderScreen({Reminder? reminder, int? index}) async {
+    Reminder? newReminder = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddReminderScreen(reminder: reminder),
+      ),
+    );
+    if (newReminder != null) {
+      setState(() {
+        if (index != null) {
+          reminders[index] = newReminder;
+        } else {
+          reminders.add(newReminder);
+        }
+      });
+      _saveReminders();
+    }
+  }
 }
 
-class Reminder {
-  String title;
-  String frequency;
-  bool isActive;
+class AddReminderScreen extends StatefulWidget {
+  final Reminder? reminder;
 
-  Reminder({
-    required this.title,
-    required this.frequency,
-    required this.isActive,
-  });
+  AddReminderScreen({Key? key, this.reminder}) : super(key: key);
+
+  @override
+  _AddReminderScreenState createState() => _AddReminderScreenState();
+}
+
+class _AddReminderScreenState extends State<AddReminderScreen> {
+  late TextEditingController _titleController;
+  late TextEditingController _frequencyController;
+  late bool _isActive;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController =
+        TextEditingController(text: widget.reminder?.title ?? '');
+    _frequencyController =
+        TextEditingController(text: widget.reminder?.frequency ?? '');
+    _isActive = widget.reminder?.isActive ?? true;
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _frequencyController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.reminder != null ? 'Edit Reminder' : 'Add Reminder'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: _titleController,
+              decoration: InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: TextStyle(color: Colors.white)),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: _frequencyController,
+              decoration: InputDecoration(
+                  labelText: 'Frequency',
+                  labelStyle: TextStyle(color: Colors.white)),
+            ),
+            SizedBox(height: 16.0),
+            CheckboxListTile(
+              title: Text('Active', style: TextStyle(color: Colors.white)),
+              value: _isActive,
+              onChanged: (value) {
+                setState(() {
+                  _isActive = value!;
+                });
+              },
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                _saveReminder();
+              },
+              child: Text('Save', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _saveReminder() {
+    String title = _titleController.text.trim();
+    String frequency = _frequencyController.text.trim();
+
+    if (title.isNotEmpty && frequency.isNotEmpty) {
+      Reminder reminder = Reminder(
+        title: title,
+        frequency: frequency,
+        isActive: _isActive,
+      );
+
+      Navigator.pop(context, reminder);
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error', style: TextStyle(color: Colors.white)),
+            content:
+                Text('Please enter a title and frequency for the reminder.'),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 }
